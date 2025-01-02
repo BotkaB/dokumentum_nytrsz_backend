046fa99a9e9afe01c0998b69dec8342a3c1f9c40
BotkaB <botikabinaka@gmail.com>
2025-01-02 12:19:20 +0100

    alap projekt users táblával

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        User::factory(10)->create();
+
+        User::factory()->create([
+            'name' => 'Test Stat',
+            'email' => 'teststat@example.com',
+            'password'=>'Pass12word',
+            'jogosultsag'=>2
+        ]);
+
+        User::factory()->create([
+            'name' => 'Test Doku',
+            'email' => 'testdoku@example.com',
+            'password'=>'Pass13word',
+            'jogosultsag'=>1
+        ]);
+        User::factory()->create([
+            'name' => 'Test Admin',
+            'email' => 'testadmin@example.com',
+            'password'=>'Pass14word',
+            'jogosultsag'=>0
+        ]);
+    }
+}
