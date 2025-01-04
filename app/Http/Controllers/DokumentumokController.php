ca9563d577d1795f734344bc0997292181f3bf42
BotkaB <botikabinaka@gmail.com>
2025-01-04 13:00:57 +0100

    dokumentumok index és show függvények

--- app/Http/Controllers/DokumentumokController.php
+++ app/Http/Controllers/DokumentumokController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Dokumentumok;
 use Illuminate\Http\Request;
 
 class DokumentumokController extends Controller
 {
-    //
+    public function index()
+    {
+        $dokumentumok = response()->json(Dokumentumok::all());
+        return $dokumentumok;
+    }
+
+    public function show($id)
+    {
+        $dokumentum = response()->json(Dokumentumok::find($id));
+        return $dokumentum;
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DokumentumokController;
 use App\Http\Controllers\MegvalositasiHelyszinController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
@@ -9,12 +10,14 @@ Route::middleware('auth:sanctum')->post('/user/update/{id}', [UserController::cl
 // Csak admin hozzáférés
 Route::middleware(['auth:api', 'role:admin'])->get('/admin', function () {
     return response()->json(['message' => 'Admin Dashboard']);
-    Route::get('/megvalositasihelyzinek', [MegvalositasiHelyszinController::class,'index']);
+    Route::get('/megvalositasihelyszinek', [MegvalositasiHelyszinController::class,'index']);
 });
 
 // Dokumentumellenőrző hozzáférés (admin és dokumentumellenőrző)
 Route::middleware(['auth:api', 'role:document_checker'])->get('/documents', function () {
     return response()->json(['message' => 'Documents Dashboard']);
+    Route::get('/dokumentumok', [DokumentumokController::class,'index']);
+    Route::get('/dokumentumok/{id}', [DokumentumokController::class,'show']);
 });
 
 // Statisztikai lekérdező hozzáférés (statisztikai lekérdező, dokumentumellenőrző és admin)
