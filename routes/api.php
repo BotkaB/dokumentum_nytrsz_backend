1b374f5377b87b44dd6364442f0cea412d0d1693
BotkaB <botikabinaka@gmail.com>
2025-01-10 17:43:08 +0100

    getUser

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,8 @@ use App\Http\Middleware\Admin;
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+//Route::middleware('auth:sanctum')->get('/user', [UserController::class, 'getUser']);
+Route::middleware('auth:sanctum')->post('/user/update/{id}', [UserController::class, 'updateUser']);
 
 Route::middleware(['auth:sanctum', Admin::class])
 ->group(function () {
