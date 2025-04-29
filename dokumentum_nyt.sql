7a31af4a8c99131baf12cbb362d2037d8aab80cd
BotkaB <botikabinaka@gmail.com>
2025-04-29 18:42:26 +0200

    adatbázis export file

--- dokumentum_nyt.sql
+++ dokumentum_nyt.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2025. Ápr 29. 18:03
+-- Létrehozás ideje: 2025. Ápr 29. 18:23
 -- Kiszolgáló verziója: 10.4.32-MariaDB
 -- PHP verzió: 8.2.12
 
